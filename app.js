d0ce9904d2702f416211e7fbc8fb8f0ec4afdd36
uttam-geek <panasalauttam@gmail.com>
2017-01-21 13:57:28 +0530

    Conversation added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+//Watson SDK
+var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
 
 
 //Routes
@@ -14,6 +16,9 @@ var users = require('./routes/users');
 //Cloud connect
 cfenv = require("cfenv");
 appEnv   = cfenv.getAppEnv();
+
+//Get envornment credentials
+require('dotenv').config({silent: true});
 
 
 //Express Package
@@ -36,6 +41,80 @@ app.use(express.static(path.join(__dirname, 'public'))); //For resources
 app.use('/', index);
 app.use('/users', users);
 
+
+
+
+// Create the service wrapper
+var conversation = new Conversation({
+  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
+  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+  // username: '<username>',
+  // password: '<password>',
+  url: 'https://gateway.watsonplatform.net/conversation/api',
+  version_date: '2016-10-21',
+  version: 'v1'
+});
+// Endpoint to be call from the client side
+app.post('/api/message', function(req, res) {
+  //console.log(res);
+  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+  if (!workspace || workspace === '<workspace-id>') {
+    return res.json({
+      'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }
+    });
+  }
+  var payload = {
+    workspace_id: workspace,
+    context: req.body.context || {},
+    input: req.body.input || {}
+  };
+
+  // Send the input to the conversation service
+  conversation.message(payload, function(err, data) {
+    if (err) {
+      return res.status(err.code || 500).json(err);
+    }
+
+    return res.json(updateMessage(payload, data));
+  });
+});
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Conversation service
+ * @param  {Object} response The response from the Conversation service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  //console.log(response);
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  return response;
+}
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -53,17 +132,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-app.get('/process_get', function (req, res) {
-// Prepare output in JSON format
-response = {
-first_name:req.query.first_name,
-last_name:req.query.last_name
-};
-console.log(response);
-res.end(JSON.stringify(response));
-})
-
 /*Server*/
 var server = app.listen(appEnv.port, function () {
 var host = server.address().address
@@ -72,7 +140,6 @@ console.log("Example app listening at http://%s:%s", host, port)
 })
 
 
-//var watson = require('watson-developer-cloud');
-//console.log(watson);
+
 
 module.exports = app;
